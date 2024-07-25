d44c8ae0cdcdb2792e800ed9d2abf64c9c2ef3d0
jakubscholtz <jakub.scholtz@intervieweb.it>
2024-07-25 10:49:45 +0200

    fix: ip test

--- app.py
+++ app.py
@@ -6,6 +6,28 @@ st.set_page_config(
     # layout="wide",
 )
 
+
+from streamlit import runtime
+from streamlit.runtime.scriptrunner import get_script_run_ctx
+
+
+def get_remote_ip() -> str:
+    """Get remote ip."""
+
+    try:
+        ctx = get_script_run_ctx()
+        if ctx is None:
+            return None
+
+        session_info = runtime.get_instance().get_client(ctx.session_id)
+        if session_info is None:
+            return None
+    except Exception as e:
+        return None
+
+    return session_info.request.remote_ip
+
+
 import streamlit_antd_components as sac
 import base64
 import uuid
@@ -44,7 +66,9 @@ if "voted" not in st.session_state:
 st.session_state["beans"] = get_beans()
 
 
-st.title(":coffee: Caffe**in**e ")
+# st.title(":coffee: Caffe**in**e ")
+st.image("caffeine.png", use_column_width=True)
+st.toast(f"The remote ip is {get_remote_ip()}")
 st.markdown(" - Please gives us your opinion about our coffee!")
 st.markdown(" - 5 cups is the best rating, 1 cup is the worst rating.")
 st.write("After you vote, you will see the price estimate per espresso.")
